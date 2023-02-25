495961d8f0ce2b6f981e31327f5b98e5fae37db3
pophero110 <38047622+pophero110@users.noreply.github.com>
2023-02-25 11:02:49 -0600

    hw0 exercises

---
+++ hw0/DrawTriangle.java
@@ -0,0 +1,14 @@
+public class DrawTriangle {
+    public static void drawTriangle(int N) {
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        drawTriangle(10);
+    }
+}
---
+++ hw0/EnhancedForBreakDemo.java
@@ -0,0 +1,14 @@
+public class EnhancedForBreakDemo {
+    public static void main(String[] args) {
+        String[] a = {"cat", "dog", "laser horse", "ketchup", "horse", "horbse"};
+        // void creating an index variable using a special syntax involving a colon
+        for (String s : a) {
+            for (int j = 0; j < 3; j += 1) {
+                System.out.println(s);
+                if (s.contains("horse")) {
+                    break;
+                }
+            }
+        }
+    }
+}
